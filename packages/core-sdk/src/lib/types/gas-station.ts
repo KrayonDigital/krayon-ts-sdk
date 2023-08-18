4a2a75684cbb3530a82042687ce69bf68f88e973
idan-krayon <idan@krayondigital.com>
2023-08-18 18:38:44 +0300

    what do you think?

--- packages/core-sdk/src/lib/types/gas-station.ts
+++ packages/core-sdk/src/lib/types/gas-station.ts
@@ -1,25 +1,34 @@
-import { BlockchainTitle } from "./blockchain-types";
+import { Wallet } from '@krayon-digital/core-sdk';
+import { BlockchainTitle } from './blockchain-types';
 
-export interface GasStationDto {
-  data?: any;
-  name?: string;
-  blockchain: BlockchainTitle;
-  organization?: string;
-  group?: string;
+export interface GasStationDto extends Wallet {
   balance_cap?: number | string;
   red_marker?: number | string;
   max_network_fee?: number | string;
-  id?: string;
-  address?: string;
-  description?: string;
-  gas_station_status?: number;
-  image?: string;
-  balance?: string;
-  pending_usd_balance?: number;
-  total_usd_balance?: string;
-  symbol?: string;
+  data?: any;
   assets?: GasStationAssets[];
 }
+
+// export interface GasStationDto {
+//   data?: any;
+//   name?: string;
+//   blockchain: BlockchainTitle;
+//   organization?: string;
+//   group?: string;
+//   balance_cap?: number | string;
+//   red_marker?: number | string;
+//   max_network_fee?: number | string;
+//   id?: string;
+//   address?: string;
+//   description?: string;
+//   gas_station_status?: number;
+//   image?: string;
+//   balance?: string;
+//   pending_usd_balance?: number;
+//   total_usd_balance?: string;
+//   symbol?: string;
+//   assets?: GasStationAssets[];
+// }
 export interface GasStationAssets {
   balance: string;
   blockchain: string;
