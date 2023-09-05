6d15ac327072c60603405758b3d60e40bbae4976
Tal Suhareanu <tal@krayondigital.com>
2023-09-05 14:34:57 +0300

    KD-1413 add util

--- packages/walletconnect-sdk/src/util/chain-utils.ts
+++ packages/walletconnect-sdk/src/util/chain-utils.ts
@@ -1,5 +1,12 @@
 import { EIP155_CHAINS } from '../data/EIP155Data';
 
+export function isValidQualifiedChain(chainTypeAndChain: string) {
+  // chainTypeAndChain is a string like "eip155:1" (chainType:chainId)
+  // Later on, we might want to add support for other chain types, but for now only support
+  // EIP155 chains taken verbatim from the EIP155_CHAINS
+  return Object.keys(EIP155_CHAINS).includes(chainTypeAndChain.toString());
+}
+
 // Create a function that looks up a chain_id based on the blockchain
 export function getChainId(blockchain: string | number): number {
   let chainId: number | undefined;
@@ -20,13 +27,15 @@ export function getChainId(blockchain: string | number): number {
   }
   // otherwise, we use blockchain string to search
   else {
-    chainId = allChains.find((chain) => chain.blockchain === blockchain)?.chainId;
+    chainId = allChains.find(
+      (chain) => chain.blockchain === blockchain
+    )?.chainId;
   }
 
   // We still didn't find anyghing
   /*   if (chainId === undefined) {
-    throw new Error(`Unknown blockchain: ${blockchain}`);
-  } */
+        throw new Error(`Unknown blockchain: ${blockchain}`);
+      } */
 
   return chainId || 0;
 }
@@ -36,4 +45,14 @@ export function getChainIdHex(blockchain: string | number): string {
   // Convert the chainId from number to hex string
   const chainIdHex = '0x' + chainId.toString(16);
   return chainIdHex;
+}
+
+export function parseQualifiedChainId(qualifiedChainID: string): number {
+  const [_, chainIdStr] = qualifiedChainID.split(':');
+  const chainId = parseInt(chainIdStr);
+  if (!isValidQualifiedChain(qualifiedChainID)) {
+    throw new Error(`Invalid qualified chain id: ${qualifiedChainID}`);
+  }
+
+  return chainId;
 }
