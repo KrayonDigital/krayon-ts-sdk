df0c1c998ec9a27ade9d6b33e5a75cad1f46abe1
avrik <meavrik@gmail.com>
2023-10-14 12:45:52 +0300

    update schema

--- packages/core-sdk/src/lib/organization/organization-sdk.ts
+++ packages/core-sdk/src/lib/organization/organization-sdk.ts
@@ -10,10 +10,18 @@ import {
   SpendingLimitFilter,
   SubAccountResponse,
 } from '../types/organization';
-import { DataWrap, KrayonAPICommonOptions, PaginationRequest } from '../types/common';
+import {
+  DataWrap,
+  KrayonAPICommonOptions,
+  PaginationRequest,
+} from '../types/common';
 import { Asset, AssetResponse } from '../types/asset';
 import { WhitelistContract } from '../types/whitelist';
-import { CreateOrganization, InviteUser, Organization, } from '../types/organization';
+import {
+  CreateOrganization,
+  InviteUser,
+  Organization,
+} from '../types/organization';
 import { Pageable } from '../types/pagination';
 import { CreateUser, User, UserResponse } from '../types/user';
 import { UserSpendingLimit } from '../types/spending-limit';
@@ -28,80 +36,128 @@ export class KrayonOrganizationSDK {
   readonly organizationId?: string;
   readonly apiClient: KrayonAPIClient;
 
-  constructor({ organizationId, apiClient }: { organizationId?: string; apiClient: KrayonSDK['apiClient'] }) {
+  constructor({
+    organizationId,
+    apiClient,
+  }: {
+    organizationId?: string;
+    apiClient: KrayonSDK['apiClient'];
+  }) {
     this.organizationId = organizationId;
     this.apiClient = apiClient;
   }
 
-  listOrganizationUsers(params?: OrganizationUsersFilter, options?: KrayonAPICommonOptions) {
+  listOrganizationUsers(
+    params?: OrganizationUsersFilter,
+    options?: KrayonAPICommonOptions
+  ) {
     if (!this.organizationId) {
       throw new Error('Organization id is required for this operation');
     }
     const { abortSignal } = options || {};
-    return this.apiClient.get<UserResponse>(`/organizations/${this.organizationId}/users`, {
-      params,
-      signal: abortSignal,
-    });
+    return this.apiClient.get<UserResponse>(
+      `/organizations/${this.organizationId}/users`,
+      {
+        params,
+        signal: abortSignal,
+      }
+    );
   }
 
-  listOrganizationAssets(params?: OrganizationAssetsFilter, options?: KrayonAPICommonOptions) {
+  listOrganizationAssets(
+    params?: OrganizationAssetsFilter,
+    options?: KrayonAPICommonOptions
+  ) {
     if (!this.organizationId) {
       throw new Error('Organization id is required for this operation');
     }
     const { abortSignal } = options || {};
-    return this.apiClient.get<AssetResponse>(`/organizations/${this.organizationId}/assets`, {
-      params,
-      signal: abortSignal,
-    });
+    return this.apiClient.get<AssetResponse>(
+      `/organizations/${this.organizationId}/assets`,
+      {
+        params,
+        signal: abortSignal,
+      }
+    );
   }
 
   getOrganization(options?: GetOrganizationOptions) {
     // For backward compatibility, allow passing organization id here as an override
     // This should normally not be done/needed since other organization id shouldn't be accessible
-    const {abortSignal } = options || {};
+    const { abortSignal } = options || {};
     const organizationId = options?.organizationId ?? this.organizationId;
     if (!organizationId) {
       throw new Error('Organization id is required for this operation');
     }
-    return this.apiClient.get<DataWrap<Organization>>(`/organizations/${organizationId}`, { signal: abortSignal });
+    return this.apiClient.get<DataWrap<Organization>>(
+      `/organizations/${organizationId}`,
+      { signal: abortSignal }
+    );
   }
 
-  createOrganization(orgObj: CreateOrganization, extraParams?: KrayonAPICommonOptions) {
+  createOrganization(
+    orgObj: CreateOrganization,
+    extraParams?: KrayonAPICommonOptions
+  ) {
     const { abortSignal } = extraParams || {};
-    return this.apiClient.post<DataWrap<Organization>>('/organizations', orgObj, { signal: abortSignal });
+    return this.apiClient.post<DataWrap<Organization>>(
+      '/organizations',
+      orgObj,
+      { signal: abortSignal }
+    );
   }
 
-  updateOrganization( orgChangesObj: Partial<CreateOrganization>, subAccountId?: string, extraParams?: KrayonAPICommonOptions) {
+  updateOrganization(
+    orgChangesObj: Partial<CreateOrganization>,
+    subAccountId?: string,
+    extraParams?: KrayonAPICommonOptions
+  ) {
     if (!this.organizationId) {
       throw new Error('Organization id is required for this operation');
     }
     const { abortSignal } = extraParams || {};
-    const id = subAccountId ?? this.organizationId
-    return this.apiClient.patch<DataWrap<Organization>>(`/organizations/${id}`, orgChangesObj, {
-      signal: abortSignal,
-    });
+    const id = subAccountId ?? this.organizationId;
+    return this.apiClient.patch<DataWrap<Organization>>(
+      `/organizations/${id}`,
+      orgChangesObj,
+      {
+        signal: abortSignal,
+      }
+    );
   }
 
-  inviteUserToOrganization(userObj: InviteUser, extraParams?: KrayonAPICommonOptions) {
+  inviteUserToOrganization(
+    userObj: InviteUser,
+    extraParams?: KrayonAPICommonOptions
+  ) {
     if (!this.organizationId) {
       throw new Error('Organization id is required for this operation');
     }
     const { abortSignal } = extraParams || {};
-    return this.apiClient.post<DataWrap<InviteUser>>(`/organizations/${this.organizationId}/invite-user`, userObj, {
-      signal: abortSignal,
-    });
+    return this.apiClient.post<DataWrap<InviteUser>>(
+      `/organizations/${this.organizationId}/invite-user`,
+      userObj,
+      {
+        signal: abortSignal,
+      }
+    );
   }
 
-  listOrganizationInvitations(filterObj?: OrganizationInvitationsFilter, options?: KrayonAPICommonOptions) {
+  listOrganizationInvitations(
+    filterObj?: OrganizationInvitationsFilter,
+    options?: KrayonAPICommonOptions
+  ) {
     if (!this.organizationId) {
       throw new Error('Organization id is required for this operation');
     }
     const { abortSignal } = options || {};
     return this.apiClient.get<DataWrap<OrganizationInvitationsType>>(
-      `/organizations/${this.organizationId}/invitations`, {
+      `/organizations/${this.organizationId}/invitations`,
+      {
         params: filterObj,
         signal: abortSignal,
-    });
+      }
+    );
   }
 
   // TODO: check whether we should even allow passing org id? using it now for compatibility with Frontend
@@ -117,22 +173,33 @@ export class KrayonOrganizationSDK {
     }
     const { abortSignal } = extraParams || {};
     return this.apiClient.get<Pageable<UserSpendingLimit>>(
-      `/organizations/${usedOrgId}/spending-limits`, {
+      `/organizations/${usedOrgId}/spending-limits`,
+      {
         params, // this already contains all the required spending limit params less orgID
         signal: abortSignal,
-    });
+      }
+    );
   }
 
-  updateOrganizationPolicies(allowed_whitelist_recipients_only:boolean, extraParams?: KrayonAPICommonOptions) {
+  updateOrganizationPolicies(
+    allowed_whitelist_recipients_only: boolean,
+    extraParams?: KrayonAPICommonOptions
+  ) {
     const { abortSignal } = extraParams || {};
-    return this.apiClient.patch(`/organization-policies`,{allowed_whitelist_recipients_only}, {
-      signal: abortSignal,
-    });
+    return this.apiClient.patch(
+      `/organization-policies`,
+      { allowed_whitelist_recipients_only },
+      {
+        signal: abortSignal,
+      }
+    );
   }
 
   getOrganizationPolicies(extraParams?: KrayonAPICommonOptions) {
     const { abortSignal } = extraParams || {};
-    return this.apiClient.get<DataWrap<{allowed_whitelist_recipients_only:boolean}>>(`/organization-policies`, {
+    return this.apiClient.get<
+      DataWrap<{ allowed_whitelist_recipients_only: boolean }>
+    >(`/organization-policies`, {
       signal: abortSignal,
     });
   }
@@ -152,9 +219,12 @@ export class KrayonOrganizationSDK {
       throw new Error('Organization id is required for this operation');
     }
     const { abortSignal } = extraParams || {};
-    return this.apiClient.get<Pageable<Wallet>>(`/organizations/${this.organizationId}/ext-wallets`, {
-      signal: abortSignal,
-    });
+    return this.apiClient.get<Pageable<Wallet>>(
+      `/organizations/${this.organizationId}/ext-wallets`,
+      {
+        signal: abortSignal,
+      }
+    );
   }
 
   getGasStations(extraParams?: KrayonAPICommonOptions) {
@@ -162,9 +232,12 @@ export class KrayonOrganizationSDK {
       throw new Error('Organization id is required for this operation');
     }
     const { abortSignal } = extraParams || {};
-    return this.apiClient.get<Pageable<GasStationDto>>(`/organizations/${this.organizationId}/gas-stations`, {
-      signal: abortSignal,
-    });
+    return this.apiClient.get<Pageable<GasStationDto>>(
+      `/organizations/${this.organizationId}/gas-stations`,
+      {
+        signal: abortSignal,
+      }
+    );
   }
 
   updateQuorum(num_quorum: number, extraParams?: KrayonAPICommonOptions) {
@@ -176,43 +249,87 @@ export class KrayonOrganizationSDK {
     );
   }
 
-  subAccountBusiness(orgObj: CreateSubAccount, extraParams?: KrayonAPICommonOptions) {
+  subAccountBusiness(
+    orgObj: CreateSubAccount,
+    extraParams?: KrayonAPICommonOptions
+  ) {
     const { abortSignal } = extraParams || {};
-    return this.apiClient.post<DataWrap<Organization>>(`/organizations/${this.organizationId}/sub-account`, orgObj, {
-      signal: abortSignal,
-    });
+    return this.apiClient.post<DataWrap<Organization>>(
+      `/organizations/${this.organizationId}/sub-account`,
+      orgObj,
+      {
+        signal: abortSignal,
+      }
+    );
   }
 
   subAccountIndividual(user: CreateUser, extraParams?: KrayonAPICommonOptions) {
     const { abortSignal } = extraParams || {};
     const subAccountObj = { ...user, sub_account_type: 'individual' };
-    return this.apiClient.post<DataWrap<User>>(`/organizations/${this.organizationId}/sub-account`, subAccountObj, {
-      signal: abortSignal,
-    });
+    return this.apiClient.post<DataWrap<User>>(
+      `/organizations/${this.organizationId}/sub-account`,
+      subAccountObj,
+      {
+        signal: abortSignal,
+      }
+    );
   }
 
   listSubAccounts(extraParams?: KrayonAPICommonOptions) {
     const { abortSignal } = extraParams || {};
-    return this.apiClient.get<SubAccountResponse>(`/organizations/${this.organizationId}/sub-accounts`, {
-      signal: abortSignal,
-    });
+    return this.apiClient.get<SubAccountResponse>(
+      `/organizations/${this.organizationId}/sub-accounts`,
+      {
+        signal: abortSignal,
+      }
+    );
   }
   getWallets(organizationId: string, extraParams?: KrayonAPICommonOptions) {
     const { abortSignal } = extraParams || {};
-    return this.apiClient.get<Pageable<Wallet>>(`/organizations/${organizationId}/wallets`, {
-      signal: abortSignal,
-    });
+    return this.apiClient.get<Pageable<Wallet>>(
+      `/organizations/${organizationId}/wallets`,
+      {
+        signal: abortSignal,
+      }
+    );
   }
-  listSubAccountAssets(organizationId: string, extraParams?: KrayonAPICommonOptions) {
+  listSubAccountAssets(
+    organizationId: string,
+    extraParams?: KrayonAPICommonOptions
+  ) {
     const { abortSignal } = extraParams || {};
-    return this.apiClient.get<AssetResponse>(`/organizations/${organizationId}/assets`, {
-      signal: abortSignal,
-    });
+    return this.apiClient.get<AssetResponse>(
+      `/organizations/${organizationId}/assets`,
+      {
+        signal: abortSignal,
+      }
+    );
   }
 
-  companyVerification(payload: CompanyVerification, extraParams?: KrayonAPICommonOptions) {
+  companyVerification(
+    payload: CompanyVerification,
+    extraParams?: KrayonAPICommonOptions
+  ) {
     const { abortSignal } = extraParams || {};
+
+    const formData = new FormData();
+    formData.append('file', payload.documents.certificate_of_incorporation);
+    formData.append('file', payload.documents.company_proof_of_address);
+    payload.documents.ubos.forEach((a) => formData.append('file', a.file));
+    /*  const test = {
+      ...payload,
+      documents: formData,
+    }; */
+    /*    formData.append('file', payload.documents.certificate_of_incorporation);
+    formData.append('file', payload.documents.company_proof_of_address);
+    payload.documents.ubos.forEach((a) => formData.append('file', a.file)); */
+
+    /*  axios.post("http://localhost:8000/api/upload/", formData, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        } */
     return this.apiClient.post(`/kyb`, payload, {
+      headers: { 'Content-Type': 'multipart/form-data' },
       signal: abortSignal,
     });
   }
