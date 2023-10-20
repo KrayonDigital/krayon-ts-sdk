3af43caeec658feaae5ec780bce9f4ee3b0cb9f9
avrik <meavrik@gmail.com>
2023-10-20 10:45:20 +0300

    fix blob

--- packages/core-sdk/src/lib/organization/organization-sdk.ts
+++ packages/core-sdk/src/lib/organization/organization-sdk.ts
@@ -310,30 +310,26 @@ export class KrayonOrganizationSDK {
     payload: CompanyVerification,
     extraParams?: KrayonAPICommonOptions
   ) {
+    console.log('payload', payload);
     const { abortSignal } = extraParams || {};
-    const getBlob = (file: any) => {
-      return file;
+    const formData = new FormData();
+    const addFile = (name: string, file: any) => {
+      formData.append(name, file, file.name);
     };
 
-    const formData = new FormData();
-    formData.append(
-      'file',
-      payload.documents.certificate_of_incorporation,
-      payload.documents.certificate_of_incorporation.name
+    addFile(
+      'certificate_of_incorporation',
+      payload.documents.certificate_of_incorporation
     );
-    // formData.append('file', getBlob(payload.documents.company_proof_of_address.file));
-    console.log('payload', payload);
-    // payload.documents.ubos.forEach((a) => formData.append('file', getBlob(a.file)));
+    addFile(
+      'company_proof_of_address',
+      payload.documents.company_proof_of_address
+    );
+    payload.documents.ubos.forEach((file, index) =>
+      addFile('ubos' + (index + 1), file)
+    );
 
     formData.append('json', JSON.stringify(payload));
-
-    /*   let formData = new FormData();
-    formData.append("file", Buffer.from(file.data), {
-        filename: file.name,
-        contentType: file.mimetype,
-    });
- */
-    console.log('formData', formData);
 
     return this.apiClient.post(`/kyb`, formData, {
       headers: { 'Content-Type': 'multipart/form-data' },
