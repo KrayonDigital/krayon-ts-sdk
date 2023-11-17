4da01b2319ecad9db3000d252912b5646cd185d0
avrik <meavrik@gmail.com>
2023-11-17 12:07:56 +0200

    add deposit status

--- packages/core-sdk/src/lib/types/merchant.ts
+++ packages/core-sdk/src/lib/types/merchant.ts
@@ -1,14 +1,27 @@
 import { Pageable } from './pagination';
 
+export enum DepositStatus {
+  SUCCESS = 'SUCCESS',
+  ERROR = 'ERROR',
+  UNKNOWN = 'UNKNOWN',
+}
+
+export const DepositStatusName = new Map([
+  [DepositStatus.ERROR, 'Error'],
+  [DepositStatus.SUCCESS, 'Success'],
+  [DepositStatus.UNKNOWN, 'Unknown'],
+]);
+
 export interface MerchantDeposit {
   id: string;
   name: string;
   currency: string;
   logo: string;
-  date: string;
+  created_at: string;
   gross_payment: string;
   payment_fee: string;
-  net_amount: string;
+  amount: string;
+  symbol: string;
   type: string;
 }
 
@@ -27,10 +40,32 @@ export interface MerchantNotificationDeposit {
 export interface MerchantNotification {
   id: string;
   created_at: string;
-  delivery_status: string;
+  delivery_status: DepositStatus;
   delivery_error_message: string;
   related_object_details: MerchantNotificationDeposit | any;
 }
+
+export const getMerchantStatusColor = (status?: string) => {
+  const statusColor = { bg: 'bg-red/10', text: 'text-red' };
+
+  switch (status) {
+    case DepositStatus.UNKNOWN:
+      statusColor.bg = 'bg-blue/10';
+      statusColor.text = 'text-blue';
+      break;
+
+    case DepositStatus.SUCCESS:
+      statusColor.bg = 'bg-green/10';
+      statusColor.text = 'text-green';
+      break;
+
+    default:
+      statusColor.bg = 'bg-red/10';
+      statusColor.text = 'text-red';
+      break;
+  }
+  return statusColor;
+};
 
 export type MerchantDepositsResponse = Pageable<MerchantDeposit>;
 export type MerchantNotificationResponse = Pageable<MerchantNotification>;
