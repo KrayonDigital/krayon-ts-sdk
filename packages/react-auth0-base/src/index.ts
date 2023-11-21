c306401c2fa251eebf7afa7f209290f6864c5f23
Ivan <ivan.ivezic@gmail.com>
2023-11-21 10:12:43 +0100

    vu

--- packages/react-auth0-base/src/index.ts
+++ packages/react-auth0-base/src/index.ts
@@ -3,7 +3,6 @@
 import KrayonSdkClientContext from './sdkClientContext';
 
 export { KrayonSdkClientContext };
-
 export * from './use-sdk-hooks';
 export * from './components';
 export * from './auth0-sdk-provider-base';
--- packages/react-native-auth0-sdk/src/index.ts
+++ packages/react-native-auth0-sdk/src/index.ts
@@ -1,3 +1,4 @@
 export * from '../../react-auth0-base/src/index';
 // TODO: add this file
 // export * from '../../src/shared/react-native-auth0-sdk-provider';
+export * from './react-native-auth0-sdk-provider';
